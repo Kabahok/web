8482b79d80a8571ed340bd7790f23afc46f455d8
egor avdeev <workegoravd@gmail.com>
2023-10-05 18:40:13 +0300

    task1

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+    private final static int PORT = 9999;
+    private final static int POOL_SIZE = 64;
+    public static void main(String[] args) {
+        try {
+            Server server = new Server(PORT, POOL_SIZE);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
